280a619e0e8f2dcbd02480976b391b7ed65d8190
You Jung Chiang <yoyoy74662000@gmail.com>
2017-11-19 14:49:57 -0600

    Add files via upload

---
+++ java/com/example/apple/convertcurrency/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.apple.convertcurrency;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    public void ConvertFunction(View view){
+        EditText money = (EditText) findViewById(R.id.currency);
+
+        Double doublemoney = Double.parseDouble(money.getText().toString());
+
+        Double usdollar = doublemoney*30;
+
+        Toast.makeText(MainActivity.this, usdollar.toString(),Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
